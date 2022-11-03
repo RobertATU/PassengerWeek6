2f68e8c224a091b7fe4c43b4a898c227101af911
Robert <g00395612@atu.ie>
2022-11-03 22:42:35 +0000

    Updated how the constructor worked and a new title test

--- src/test/java/ie/atu/PassengerWeek6/PassengerTest.java
+++ src/test/java/ie/atu/PassengerWeek6/PassengerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PassengerTest {
     Passenger pass;
@@ -16,179 +17,16 @@ class PassengerTest {
 
     @Test
     void testTitle() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-      //  assertEquals("Hi", pass.test());
-        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms"))
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Title must be Mr, Mrs or Ms");
-        }
-
+        Passenger pass = new Passenger("Mr","Ted","1234567890","1234567",20);
+        assertEquals("Mr", pass.getTitle());
+        System.out.println(pass);
     }
+
     @Test
     void testTitleFail() {
-        String title = "Mar";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms"))
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Title must be Mr, Mrs or Ms");
-        }
-
-    }
-    @Test
-    void testName() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(name.length() >= 3)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of three characters required");
-        }
-
-    }
-    @Test
-    void testNameFail() {
-        String title = "Mr";
-        String name = "Te";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(name.length() >= 3)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of three characters required");
-        }
-
-    }
-
-    @Test
-    void testID() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(id.length() >= 10)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of ten characters required");
-        }
-
-    }
-
-    @Test
-    void testIDFail() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123";
-        String phone = "123456789";
-        int age = 20;
-        if(id.length() >= 10)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of ten characters required");
-        }
-
-    }
-
-    @Test
-    void testPhone() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(id.length() >= 7)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of seven characters required");
-        }
-
-    }
-
-    @Test
-    void testPhoneFail() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "1234";
-        int age = 20;
-        if(id.length() >= 7)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Minimum of seven characters required");
-        }
-
-    }
-    @Test
-    void testage() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 20;
-        if(age > 16)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Must be over 16 to fly");
-        }
-
-    }
-
-    @Test
-    void testageFail() {
-        String title = "Mr";
-        String name = "Ted";
-        String id = "123345";
-        String phone = "123456789";
-        int age = 15;
-        if(age > 16)
-        {
-            pass = new Passenger(title, name, id, phone, age);
-        }
-
-        else{
-            throw new IllegalArgumentException("Must be over 16 to fly");
-        }
-
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> new Passenger("Mar", "Ted", "1234567890", "1234567", 20));
+        assertEquals("Title must be Mr, Mrs or Ms", exMessage.getMessage());
+        System.out.println(exMessage.getMessage());
     }
 
 
